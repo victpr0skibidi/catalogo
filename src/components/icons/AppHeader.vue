2c08b5e0d501e80f3d16d7e57f86f5b8da1f6a27
victpr0skibdi <victormillnitz@gmail.com>
2026-05-12 11:56:59 -0300

    12.05

---
+++ src/components/icons/AppHeader.vue
@@ -0,0 +1,41 @@
+<script setup>
+    import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+    <header>
+        <nav>
+            <RouterLink to="/">Home</RouterLink>
+            <RouterLink to="/alimentos">Alimentos</RouterLink>
+            <RouterLink to="/acessorios">Acessórios</RouterLink>
+            <RouterLink to="/brinquedos">Brinquedos</RouterLink>
+            <RouterLink to="/higiene">Higiene</RouterLink>
+        </nav>
+    </header>
+</template>
+
+<style scoped>
+header{
+    position: fixed;
+    top: 0; left: 0;
+    width: 100%;
+    background: #f8f8f8;
+    z-index: 1000;
+    padding: 16px;
+    border-bottom: 1px solid #ddd;
+    box-sizing: border-box;
+}
+
+nav{
+    display: flex;
+    gap: 15px;
+}
+a{
+    text-decoration: none;
+    color: #333;
+}
+.router-link-active{
+    font-weight: bold;
+}
+</style>
+
